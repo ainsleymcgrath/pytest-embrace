db00365169f4b501c2e4c8b88a25526f27371759
Ains (Remote) <ainsley@amper.xyz>
2022-06-30 17:55:27 -0500

    feat: add '--embrace-ls' flag

--- pytest_embrace/plugin.py
+++ pytest_embrace/plugin.py
@@ -1,3 +1,5 @@
+from textwrap import dedent
+
 import pytest
 from pyperclip import copy
 
@@ -24,25 +26,48 @@ def pytest_generate_tests(metafunc: pytest.Metafunc) -> None:
 
 
 def pytest_addoption(parser: pytest.Parser) -> None:
-    parser.addoption("--embrace", help="")
-    parser.addoption("--embrace-table")
-    parser.addoption("--embrace-doc")
+    parser.addoption(
+        "--embrace",
+        dest="caller_fixture_name",
+        help=dedent(
+            "Takes the name of an Embrace() caller fixture and generates scaffolding"
+            " for a new test module that will use it."
+            "\nRun `pytest --embrace-ls` to see all the available caller fixtures."
+        ),
+    )
+    parser.addoption(
+        "--embrace-ls",
+        help=dedent("List all the fixtures created for calling Embrace() test suites."),
+        action="store_true",
+    )
+
+
+STOP_LOOP = object()  # for pytest hooks that stop on the first-non-None-result
 
 
 def pytest_runtestloop(session: pytest.Session) -> object:
     generate_for = session.config.getoption("--embrace")
-    if generate_for is None:
-        return None
+    do_ls = session.config.getoption("--embrace-ls")
 
-    registry_ = registry()
-    if generate_for not in registry_:
-        pytest.exit(
-            f"No such fixture '{generate_for}'."
-            f" Your options are {sorted([*registry_])}"
-        )
+    if generate_for is None and do_ls is None:
+        return None  # allow normal pytest to happen
 
-    copypasta = gen_text(generate_for)
-    print(f"\nCopying the following output to your clipboard:\n{copypasta}")
-    copy(copypasta)
+    reg = registry()
 
-    return object()  # stop the loop
+    if generate_for is not None:
+        if generate_for not in reg:
+            pytest.exit(
+                f"No such fixture '{generate_for}'."
+                f" Your options are {sorted([*reg])}"
+            )
+
+        copypasta = gen_text(generate_for)
+        print(f"\nCopying the following output to your clipboard:\n{copypasta}")
+        copy(copypasta)
+
+    if do_ls is True:
+        print()
+        print(f"The following {len(reg)} Embrace() fixtures are abailable:\n")
+        print("\n".join(f"{name}  (via {cls})" for name, cls in reg.items()))
+
+    return STOP_LOOP
