007f67b34f7e0903566be019cdc3acdba0ced634
Ains (Remote) <ainsley@amper.xyz>
2022-07-05 09:24:43 -0500

    refactor: ceiling door for potential upcoming tests

--- tests/test_plugin/utils.py
+++ tests/test_plugin/utils.py
@@ -6,7 +6,23 @@ def generated_module_stdout_factory(
     fixture: str,
     case_type: str,
     imports: str = "from pytest_embrace import CaseArtifact",
+    typical_table_kwargs: List[str] = None,
 ) -> List[str]:
+
+    typical_table_kwargs = (
+        []
+        if typical_table_kwargs is None
+        else [
+            "",
+            "",
+            "# table = [",
+            f"#    {case_type}(",
+            *[f"#        {kwarg}=..." for kwarg in typical_table_kwargs],
+            "#     )",
+            "# ]",
+        ]
+    )
+
     return [
         "Copying the following output to your clipboard:",
         "",
@@ -16,6 +32,7 @@ def generated_module_stdout_factory(
         "",
         "",
         *type_hints,
+        *typical_table_kwargs,
         "",
         "",
         f"def test({fixture}: CaseArtifact[{case_type}]) -> None:",
