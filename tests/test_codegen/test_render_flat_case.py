80a45bc105e0ad863d0e064de29480a7eb542ada
Ainsley McGrath <mcgrath.ainsley@gmail.com>
2023-03-10 13:01:05 -0600

    test: document more gotchas as tests

--- tests/test_codegen/test_render_flat_case.py
+++ tests/test_codegen/test_render_flat_case.py
@@ -63,3 +63,47 @@ def test_with_values(
             ...
         """,
     )
+
+
+def test_with_values_skips_defaults(
+    assert_valid_text_is: ValidPythonAssertion, renderer: CaseRenderUnderTest
+) -> None:
+    values = Case(number=5, word="hello")  # when default is excluded, it never renders
+
+    assert_valid_text_is(
+        renderer.with_values(values),
+        """
+        from pytest_embrace import CaseArtifact
+        from tests.test_codegen.test_render_flat_case import Case
+
+        number: int = 5
+        word: str = "hello"
+
+
+        def test(case: CaseArtifact[Case]) -> None:
+            ...
+        """,
+    )
+
+
+def test_with_values_skips_defaults_even_when_passed(
+    assert_valid_text_is: ValidPythonAssertion, renderer: CaseRenderUnderTest
+) -> None:
+    # empty items list is default
+    # quirky, but it still won't render even though it was passed explicitly.
+    values = Case(number=5, word="hello", items=[])
+
+    assert_valid_text_is(
+        renderer.with_values(values),
+        """
+        from pytest_embrace import CaseArtifact
+        from tests.test_codegen.test_render_flat_case import Case
+
+        number: int = 5
+        word: str = "hello"
+
+
+        def test(case: CaseArtifact[Case]) -> None:
+            ...
+        """,
+    )
